e1c1cca6991be2acb4a16e70ed080bfe0523f8e3
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-10-13 13:12:59 -0300

    feat: add projects list

---
+++ src/utils/projects.ts
@@ -0,0 +1,262 @@
+type Project = {
+  title: string;
+  name?: string;
+  description: string;
+  link?: string;
+};
+
+export const projects: Project[] = [
+  {
+    title: 'FATEC API (4º semestre)',
+    name: 'Cloud Fox',
+    description: 'Sistema para ver dados meteorológicos (com montagem de uma estação)',
+    link: 'https://github.com/The-Bugger-Ducks/cloud-fox-documentation',
+  },
+  {
+    title: 'Agenda',
+    name: 'Agenda para Esteticistas',
+    description: 'App para agendamento de serviços, cadastro de pacotes e histórico de clientes',
+    link: 'https://github.com/The-Bugger-Ducks/cloud-fox-documentation',
+  },
+  {
+    title: 'Portfólio Pessoal',
+    name: 'Portfólio pessoal',
+    description: 'Site com minhas informações, formação acadêmica, experiências e projetos!',
+    link: 'https://github.com/MariaGabrielaReis/MariaGabrielaReis.github.io',
+  },
+  {
+    title: 'Coins Master',
+    name: 'Coins Master (Mobile)',
+    description: 'Uma forma fácil de distribuir moedas do API (disciplina de Prog. Mobile na FATEC)',
+    link: 'https://github.com/MariaGabrielaReis/coins-master',
+  },
+  {
+    title: 'Coins Master',
+    name: 'Coins Master (API)',
+    description: 'API do projeto Coins Master (disciplina de Prog. Mobile na FATEC)',
+    link: 'https://github.com/MariaGabrielaReis/coins-master-api',
+  },
+  {
+    title: 'Curso: JStack',
+    name: 'My Contacts',
+    description: 'Aplicação desenvolvida no curso JStack (do Mateus Silva)',
+    link: 'https://github.com/MariaGabrielaReis/jstack',
+  },
+  {
+    title: 'FATEC API (3º semestre)',
+    name: 'Help Duck',
+    description: 'Sistema para Help Desk com 3 usuários: comum, suporte e administrador',
+    link: 'https://github.com/The-Bugger-Ducks/help-duck-documentation',
+  },
+  {
+    title: 'Autobots (DW III)',
+    name: 'Autobots',
+    description: 'Microsserviço para gerenciar loja de autopeças (disciplina de Desenvolvimento Web III)',
+    link: 'https://github.com/MariaGabrielaReis/DWIII-autobots',
+  },
+  {
+    title: 'Atlantis (TP II)',
+    name: 'Atlantis',
+    description: 'Sistema para gerenciar clientes de parques aquáticos (disciplina de Técnicas de Prog. II)',
+    link: 'https://github.com/MariaGabrielaReis/TPII-atlantis',
+  },
+  {
+    title: 'Jornada Dev Pleno',
+    name: 'Design Pattern: Factory Method',
+    description: 'Evento do Bruno Braga sobre "conceitos chave" do TypeScript',
+    link: 'https://github.com/MariaGabrielaReis/factory-method-design-pattern-example',
+  },
+  // 2021
+  {
+    title: '2021',
+    description: 'Projetos acadêmicos, pessoais e de eventos criados em 2021',
+  },
+  {
+    title: 'World Beauty (TP I)',
+    name: 'World Beauty',
+    description: 'Sistema para gerenciar um salão de beleza (disciplina de Técnica de Prog. I)',
+    link: 'https://github.com/MariaGabrielaReis/TPI-test',
+  },
+  {
+    title: 'FATEC API (2º semestre)',
+    name: 'Polaris',
+    description: 'Site para consumo de imagens de satélites em repositórios em nuvem',
+    link: 'https://github.com/Equipe-Polaris-DSM-2021/docs',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #07',
+    name: 'Heat: React Native',
+    description: 'Aplicação mobile para mandar sua expectativa quanto ao Do While 2021!',
+    link: 'https://github.com/MariaGabrielaReis/nlw-07-heat-react-native',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #07',
+    name: 'Heat: React',
+    description: 'Aplicação web para mandar sua expectativa quanto ao Do While 2021!',
+    link: 'https://github.com/MariaGabrielaReis/nlw-07-heat-react',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #07',
+    name: 'Heat: Node',
+    description: 'API para mandar sua expectativa quanto ao Do While 2021!',
+    link: 'https://github.com/MariaGabrielaReis/nlw-07-heat-node',
+  },
+  {
+    title: 'IMERSÃO DEV #2',
+    name: 'Imersão Dev',
+    description: 'Evento da Alura: pequenos projetos práticos em JavaScript',
+    link: 'https://github.com/MariaGabrielaReis/imersao-dev-2',
+  },
+  {
+    title: 'Semana JS Expert #5',
+    name: 'Semana JavaScript Expert #5',
+    description: 'Erick Wendel aborda conceitos do JS e TDD para criar um clone do Google Drive',
+    link: 'https://github.com/MariaGabrielaReis/semana-javascript-expert-5',
+  },
+  {
+    title: 'Ignite',
+    name: 'Aulas do Ignite',
+    description: 'Aplicações feitas no Ignite (curso da Rocketseat)',
+    link: 'https://github.com/MariaGabrielaReis/ignite-classes',
+  },
+  {
+    title: 'FATEC API (1º semestre)',
+    name: 'Análise de dados eleitorais',
+    description: 'Sistema para Help Desk com 3 usuários: comum, suporte e administrador',
+    link: 'https://github.com/Equipe-01-DSM-2021/projeto-integrador-2021-1',
+  },
+  {
+    title: 'Ignite',
+    name: 'Oficina de testes com JS',
+    description: 'Iniciando com testes em JavaScript',
+    link: 'https://github.com/MariaGabrielaReis/oficina-testes-javascript',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #06',
+    name: 'Gameplay',
+    description: 'App para organizar suas jogatinas com os amigos! ',
+    link: 'https://github.com/MariaGabrielaReis/nlw-06-gameplay',
+  },
+  {
+    title: 'Python para Zumbis',
+    name: 'Python for Zombies',
+    description: 'Exercícios de Python propostos pelo professor Fernando Masanori',
+    link: 'https://github.com/MariaGabrielaReis/Python-for-Zombies',
+  },
+  {
+    title: 'Portfólio pessoal (v.1)',
+    name: 'Portfólio pessoal',
+    description: 'Trabalho feito para a disciplina de Design Digital na FATEC',
+    link: 'https://github.com/MariaGabrielaReis/meu-portfolio',
+  },
+  {
+    title: 'Multistack (Treina Web)',
+    name: 'E-Diaristas (Mobile)',
+    description: 'App para achar o profissional de limpeza domiciliar mais perto de você',
+    link: 'https://github.com/MariaGabrielaReis/multistack-ediaristas-mobile',
+  },
+  {
+    title: 'Multistack (Treina Web)',
+    name: 'E-Diaristas (API)',
+    description: 'API para o "Multistack" (Treina Web): E-Diaristas',
+    link: 'https://github.com/MariaGabrielaReis/multistack-ediaristas-api',
+  },
+  {
+    title: 'Multistack (Treina Web)',
+    name: 'E-Diaristas (Painel Administrativo)',
+    description: 'Painel Administrativo para o "Multistack" (Treina Web): E-Diaristas',
+    link: 'https://github.com/MariaGabrielaReis/multistack-ediaristas-painel-administrativo',
+  },
+  {
+    title: 'Multistack (Treina Web)',
+    name: 'E-Diaristas (Site)',
+    description: 'Site para o "Multistack" (Treina Web): E-Diaristas',
+    link: 'https://github.com/MariaGabrielaReis/multistack-ediaristas-web',
+  },
+  {
+    title: 'Samsung Ocean',
+    name: 'Web Scraping',
+    description: 'Evento do Samsung Ocean, com foco em Web Scraping com Python',
+    link: 'https://github.com/MariaGabrielaReis/web-scraping-samsung-ocean',
+  },
+  {
+    title: 'IMERSÃO DADOS #03',
+    name: 'Imersão dados #03',
+    description: 'Evento da Alura, com foco na análise de dados com Python',
+    link: 'https://github.com/MariaGabrielaReis/imersao-dados-3',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #05',
+    name: 'Plant Manager',
+    description: 'App de lembretes (para regar suas plantinhas)',
+    link: 'https://github.com/MariaGabrielaReis/nlw-05-plant-manager',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #05',
+    name: 'Support Chat',
+    description: 'API do para chat de mensagens instantâneas para suporte a algum produto',
+    link: 'https://github.com/MariaGabrielaReis/nlw-05-support-chat',
+  },
+  {
+    title: 'MARATONA DISCOVER #02',
+    name: 'Jobs Calc',
+    description: 'Calculadora para Freelancers',
+    link: 'https://github.com/MariaGabrielaReis/maratona-discover-jobs-calc',
+  },
+  {
+    title: 'IMERSÃO DEV',
+    name: 'Imersão dev',
+    description: 'Evento da Alura, com foco em pequenos projetos práticos em JS',
+    link: 'https://github.com/MariaGabrielaReis/imersao-dev',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #04',
+    name: 'Net Promote Score',
+    description: 'API para calcular a satisfação de clientes em uma pesquisa enviada por e-mail.',
+    link: 'https://github.com/MariaGabrielaReis/nlw-04-net-promoter-score',
+  },
+  {
+    title: 'MARATONA DISCOVER',
+    name: 'Dev Finances',
+    description: 'Painel de controle de finanças',
+    link: 'https://github.com/MariaGabrielaReis/maratona-discover-dev-finances',
+  },
+  // 2020
+  {
+    title: '2020',
+    description: 'Projetos acadêmicos, pessoais e de eventos criados em 2020',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #03',
+    name: 'Happy',
+    description: 'Plataforma que facilita a busca por orfanatos',
+    link: 'https://github.com/MariaGabrielaReis/nlw-03-happy',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #02',
+    name: 'Proffy',
+    description: 'Plataforma de estudos online que conecta alunos a professores',
+    link: 'https://github.com/MariaGabrielaReis/nlw-02-proffy',
+  },
+  {
+    title: 'NEXT LEVEL WEEK #01',
+    name: 'Ecoleta',
+    description: 'Site para encontrar pontos de coleta de resíduos orgânicos e inorgânicos',
+    link: 'https://github.com/MariaGabrielaReis/nlw-01-ecoleta',
+  },
+  {
+    title: 'TCC',
+    name: 'Lab Control',
+    description: 'Sistema para reportagem de avarias em laboratórios de informática',
+  },
+  {
+    title: 'Aulas de POO',
+    name: 'Lanchonete',
+    description: 'Sistema C# para uma lanchonete',
+  },
+  {
+    title: 'Aulas de POO',
+    name: 'Livraria',
+    description: 'Sistema C# para uma livraria',
+  },
+];
